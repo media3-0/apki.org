1ca823ac34583b5768d9bb1d0a8eeabfa86e6cec
Jakub Syty <jakub.nekro@gmail.com>
2015-06-12 14:11:28 +0200

    Typescript testing (+ node installer for vagrant)

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,5 @@
+class TestController < ApplicationController
+	def test
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'test/test'
+  root 'test#test'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
