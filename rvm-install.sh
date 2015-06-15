80973cb7866b7514ab8b5dfa56191af504f0306d
Jakub Syty <jakub.nekro@gmail.com>
2015-06-15 13:12:32 +0200

    Scripts fix

--- rvm-install.sh
+++ rvm-install.sh
@@ -6,16 +6,9 @@ rvm install 2.2
 
 echo "Done (don't mind an error, it's not a bug it's a feature!"
 
-echo "Setting up 2.2 as default"
+echo "Set up 2.2 as default. Execute command:"
+echo "  rvm use 2.2 --default --create"
 
-rvm use 2.2 --default --create
-
-echo "Done"
-
-echo "Installing rails dependencies"
-
-gem install rails
-
-gem install bundler
-
-echo "Finished installing!"
+echo "Then install rails dependencies. Execute commands:"
+echo "  gem install rails"
+echo "  gem install bundler"
