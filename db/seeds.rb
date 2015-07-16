95c4f35395ca09023166e734eb0109b0c473aee5
Jakub Syty <jakub.nekro@gmail.com>
2015-07-16 10:24:05 +0200

    Seed database for quiz testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,56 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-puts 'Tworzenie użytkowników'
-10.times do |n|
-  User.create!(uid: n.to_s, nickname: "nickname#{n}", account_type: :student)
+# puts 'Tworzenie użytkowników'
+# 10.times do |n|
+#   User.create!(uid: n.to_s, nickname: "nickname#{n}", account_type: :student)
+# end
+# puts 'Użytkownicy stworzeni'
+
+Course::CourseDatum.destroy_all
+Course::Lesson.destroy_all
+Course::Quiz.destroy_all
+Course::Exercise.destroy_all
+Course::Achievement.destroy_all
+
+course = Course::CourseDatum.new(data: { 'name' => 'sample course', 'finished' => true })
+
+lesson = Course::Lesson.new(data: { 'name' => 'sample lesson 1' })
+
+quizzes = []
+quizzes << Course::Quiz.create!(data: {'question' => 'Test question 1', 'answer_idx' => 3})
+quizzes << Course::Quiz.create!(data: {'question' => 'Test question 2', 'answer_idx' => 0})
+quizzes << Course::Quiz.create!(data: {'question' => 'Test question 3', 'answer_idx' => 1})
+lesson.course_quizs.concat quizzes
+quizzes.each do |quiz|
+  quiz.save!
 end
-puts 'Użytkownicy stworzeni'
 
-@course = Course::CourseDatum.new(data: @data)
-lesson = Course::Lesson.new(data: @data)
-
-@quizzes = []
-@quizzes << Course::Quiz.create!(data: {'question' => 'Test question 1', 'answer_idx' => 3})
-@quizzes << Course::Quiz.create!(data: {'question' => 'Test question 2', 'answer_idx' => 0})
-@quizzes << Course::Quiz.create!(data: {'question' => 'Test question 3', 'answer_idx' => 1})
-lesson.course_quizs.concat @quizzes
-@quizzes.each do |quiz|
-	quiz.save!
-end
-
-Course::Achievement.create!(data: @data, lesson_id: lesson.id.to_s)
-@course.course_lessons << lesson
+Course::Achievement.create!(data: {'name' => 'Achievement 1' }, lesson_id: lesson.id.to_s)
+course.course_lessons << lesson
 lesson.save!
-@course.save!
+
+lesson = Course::Lesson.new(data: { 'name' => 'sample lesson 2' })
+
+quizzes = []
+quizzes << Course::Quiz.create!(data: {'question' => 'Test question 4', 'answer_idx' => 5})
+quizzes << Course::Quiz.create!(data: {'question' => 'Test question 5', 'answer_idx' => 2})
+quizzes << Course::Quiz.create!(data: {'question' => 'Test question 6', 'answer_idx' => 0})
+lesson.course_quizs.concat quizzes
+quizzes.each do |quiz|
+  quiz.save!
+end
+
+Course::Achievement.create!(data: {'name' => 'Achievement 2' }, lesson_id: lesson.id.to_s)
+course.course_lessons << lesson
+lesson.save!
+
+
+course.save!
+
+user = User.where(uid: 'test').exists? ? User.find_by(uid: 'test') : User.create!(name: 'test', uid: 'test', account_type: :student)
+
+Course::UserCourse.destroy_all
+Course::UserCourse.create!(course_course_datum: course, user: user)
+
+user.save!
