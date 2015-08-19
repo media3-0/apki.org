321a95639055a1354cec6ce1babfe9017f9405d9
Jakub Syty <jakub.nekro@gmail.com>
2015-08-19 12:16:31 +0200

    Add project management to rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -39,5 +39,5 @@ RailsAdmin.config do |config|
     # history_show
   end
 
-  config.included_models = %w(User News School EducatorNews Article)
+  config.included_models = %w(User News School EducatorNews Article Project)
 end
