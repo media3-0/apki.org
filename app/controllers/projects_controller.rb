6f5eb634bf0bc5000f74ff2dda6483590dbb92c9
Jakub Syty <jakub.nekro@gmail.com>
2015-09-03 11:18:28 +0200

    Minior bugfixes
    * Fixed saving icon to database
    * Link to „mine projects” only for logged in user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -90,6 +90,6 @@ class ProjectsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def project_params
-    params.require(:project).permit(:title, :github)
+    params.require(:project).permit(:title, :icon, :github)
   end
 end
